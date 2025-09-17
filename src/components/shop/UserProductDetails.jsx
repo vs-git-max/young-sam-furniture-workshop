ac64e696cdc5a2d830afb7896cd31cc4ada81b69
sam chris <vsgitsam25@gmail.com>
2025-09-17 19:03:07 +0300

    working on the dialog

--- src/components/shop/UserProductDetails.jsx
+++ src/components/shop/UserProductDetails.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Dialog } from "../ui/dialog";
 
-const UserProductDetails = () => {
-  return <div>UserProductDetails</div>;
+const UserProductDetails = ({ openProductDialog, setOpenProductDialog }) => {
+  return <Dialog>UserProductDetails</Dialog>;
 };
 
 export default UserProductDetails;
