dc959c09b63b72d5feb33d52bb5927df01ae02fb
sam chris <vsgitsam25@gmail.com>
2025-09-22 10:18:47 +0300

    corrected the footer file

--- src/components/shop/ContactForm.jsx
+++ src/components/shop/ContactForm.jsx
@@ -1,7 +1,19 @@
-import React from "react";
 import Logo from "./Logo";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+  const initialFormData = {
+    email: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  console.log(formData);
+
   return (
     <div className=" absolute  lg:inset-20 flex items-center justify-between  w-full lg:max-w-5xl mx-auto bg-green-950  py-12 px-8 lg:shadow-lg lg:shadow-white rounded-2xl lg:w-2/3 lg:max-h-5/12 lg:h-auto gap-5">
       <div className="flex-1/2">
@@ -16,8 +28,14 @@ const ContactForm = () => {
         <h1 className="text-xl lg:text-2xl text-center font-bold text-orange-700">
           Subscribe to our news letter
         </h1>
-        <form action="" className="min-w-full flex flex-col gap-5">
+        <form
+          onSubmit={onSubmit}
+          action=""
+          className="min-w-full flex flex-col gap-5"
+        >
           <input
+            value={formData.email}
+            onChange={(e) => setFormData(e.target.value)}
             type="email"
             placeholder="Enter your email..."
             className="w-full bg-green-50 rounded-xl py-2 px-4 outline-none caret-green-950 text-md font-semibold text-green-950"
--- src/components/shop/Footer.jsx
+++ src/components/shop/Footer.jsx
@@ -1,15 +1,13 @@
-import React from "react";
 import ContactForm from "./ContactForm";
 import Logo from "./Logo";
 import { footerNav, socialLinks } from "@/helper";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className=" w-full lg:mx-auto relative max-h-screen mt-12 bg-green-950  min-h-[1100px]  overflow-hidden">
+    <footer className=" w-full lg:mx-auto relative max-h-screen mt-12 bg-green-950  min-h-[1260px]  overflow-hidden">
       <div className="absolute inset-0 w-full bg-green-50 origin-top-left -skew-y-12 h-5/12"></div>
       <ContactForm />
-      <div className="lg:my-12 absolute bottom-5 top-[50%] left-0 right-0  px-2">
+      <div className="lg:my-12 absolute bottom-5  top-[50%] left-0 right-0  px-2">
         <div className="flex flex-col items-center">
           <Logo />
           <p className="text-green-50 font-bold text-sm max-w-3xl text-center">
@@ -35,17 +33,17 @@ const Footer = () => {
             })}
           </div>
         </div>
-        <div className="flex mt-12 w-full justify-between items-start lg:max-w-7xl mx-auto">
+        <div className="flex mt-12  w-full justify-between items-start lg:max-w-7xl mx-auto">
           {footerNav.map((foot) => (
             <div key={foot.title} className="">
               <h1 className="text-orange-600 font-bold text-lg">
                 {foot.title}
               </h1>
-              <div className="flex flex-col items-center gap-3">
+              <div className="flex flex-col items-start  gap-3">
                 {foot.links.map((footLink) => (
                   <a
                     href={footLink.href}
-                    className="text-green-50 font-semibold hover:underline hover:text-green-200 transition-all duration-150"
+                    className="text-green-50 font-semibold hover:underline hover:text-green-200 transition-all duration-150 "
                   >
                     {footLink.name}
                   </a>
@@ -54,6 +52,11 @@ const Footer = () => {
             </div>
           ))}
         </div>
+      </div>
+      <div className="border-t border-green-600 pt-8 absolute bottom-0 right-0 left-0">
+        <p className="text-center text-green-200 text-sm">
+          ©{new Date().getFullYear()} Furnisort. All rights reserved.
+        </p>
       </div>
     </footer>
   );
