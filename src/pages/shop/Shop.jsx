3b27a1478903138a0934c80621fa5a22eab1a75f
sam chris <vsgitsam25@gmail.com>
2025-09-23 14:32:09 +0300

    working on the shop thing

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -10,29 +10,32 @@ const Shop = () => {
   console.log(location.pathname);
 
   return (
-    <section className="py-12 w-full min-h-screen overflow-hidden px-3 relative">
-      <div className="flex items-center justify-between w-full px-8 ">
-        <h1 className="text-3xl text-green-900 font-bold  lg:text-5xl text-center">
-          Our Products
-        </h1>
-        <p className="text-orange-600 font-semibold text-lg">
-          {userProducts?.length}{" "}
-          <span className="text-green-950">Products</span>{" "}
-        </p>
+    <section className="pt-10 flex flex-col">
+      <div className="fixed z-1000 flex justify-between w-full px-5 bg-green-100 items-center">
+        <h1 className="">Our Products</h1>
+        <div className="sticky top-20  z-40 p-3 border-b">sam chris mboya</div>
       </div>
-      <div className="flex items-center justify-end ">.Sort by</div>
-      <div className="flex gap-3 ">
-        <div className=" bg-green-100 w-32  h-screen fixed ">
-          <h2 className="">Shop By Category</h2>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 lg:grid-cols-4 gap-3 lg:gap-4 pl-32">
-          {userProducts.length > 1 &&
-            userProducts &&
-            userProducts.map((product) => (
-              <UserProductCard product={product} key={product?.id} />
-            ))}
-        </div>
-      </div>
+      <aside className="fixed top-22 left-0 w-40 bg-green-50 p-5 rounded-r-lg h-[calc(100vh-7rem)] overflow-y-auto">
+        <h1>Sort by categories</h1>
+        <label className="flex items-center gap-2">
+          <input type="checkbox" /> Electronics
+        </label>
+        <label className="flex items-center gap-2">
+          <input type="checkbox" /> Fashion
+        </label>
+        <label className="flex items-center gap-2">
+          <input type="checkbox" /> Beauty
+        </label>
+      </aside>
+
+      <main className="ml-40 mt-10 p-4 grid grid-cols-[repeat(auto-fill,minmax(220px,1fr))] gap-4">
+        {userProducts?.length > 0 &&
+          userProducts &&
+          userProducts.map((product) => (
+            <UserProductCard product={product} key={product?.id} />
+          ))}
+      </main>
+
       <UserProductDetails
         openProductDialog={openProductDialog}
         setOpenProductDialog={setOpenProductDialog}
