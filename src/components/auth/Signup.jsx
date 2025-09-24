0cfb2eda4a139a465865bcac1da4559d72adac09
sam chris <vsgitsam25@gmail.com>
2025-09-24 03:08:32 +0300

    working on the signup and some redux toolkit

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Form from "@/common/Form";
+import { registerFormControls } from "@/helper";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const initialFormData = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const onSubmit = () => {};
+
+  return (
+    <div>
+      <Form
+        formControls={registerFormControls}
+        buttonText="Signup"
+        onSubmit={onSubmit}
+        formData={formData}
+        setFormData={setFormData}
+      />
+
+      <p className="">
+        Already have an account <Link to={"/auth/login"}>Login</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Signup;
